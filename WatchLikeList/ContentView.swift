1889001547a0191a08ebca6178254ccbbdf91ce0
Shen <863461783@qq.com>
2024-02-04 11:32:49 +0800

    Initial Commit

---
+++ WatchLikeList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchLikeList
+//
+//  Created by 香饽饽zizizi on 2024/2/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchLikeList/WatchLikeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLikeListApp.swift
+//  WatchLikeList
+//
+//  Created by 香饽饽zizizi on 2024/2/4.
+//
+
+import SwiftUI
+
+@main
+struct WatchLikeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
